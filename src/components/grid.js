50e93a67f34864bde8f1f629c4d11d3d601cf44b
Quinten Clause <quintenclause@hotmail.com>
2021-12-06 17:46:19 +0100

    Center grid

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,22 +1,30 @@
-let update = c => {
-};
-
 let draw = c => {
     let {ctx} = c;
     let {nodes, width, tileSize} = c.component;
     ctx.fillStyle = '#fff';
+    let offsetX = (c.entities.viewport.resize.width - c.entities.viewport.resize.minWidth) / 2;
+    let offsetY = (c.entities.viewport.resize.height - c.entities.viewport.resize.minHeight) / 2;
     nodes.forEach((node, index) => {
         let x = index % width;
         let y = index / width | 0;
         if (node === 1) {
-            ctx.fillRect(x * tileSize, y * tileSize, tileSize, tileSize);
+            ctx.fillRect(
+                offsetX + x * tileSize,
+                offsetY + y * tileSize,
+                tileSize,
+                tileSize
+            );
         } else {
-            ctx.fillRect((x + 7 / 16) * tileSize, (y + 7 / 16) * tileSize, tileSize / 8, tileSize / 8);
+            ctx.fillRect(
+                offsetX + (x + 7 / 16) * tileSize,
+                offsetY + (y + 7 / 16) * tileSize,
+                tileSize / 8,
+                tileSize / 8
+            );
         }
     });
 };
 
 export default Object.freeze({
-    draw,
-    update
+    draw
 });
